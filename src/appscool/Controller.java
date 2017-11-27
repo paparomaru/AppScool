3f38f5f4524782c37a1fef3212d3e044dadab20b
vinokurov63 <vinokurov63@gmail.com>
2017-11-27 22:36:54 +0400

    Сохранение параметров (запись в файл ини)

--- src/appscool/Controller.java
+++ src/appscool/Controller.java
@@ -1,4 +1,59 @@
 package appscool;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Controller {
+
+    @FXML
+    public TextField tfFamilia;
+
+    @FXML
+    private void processClearFam(ActionEvent event) {
+        tfFamilia.setText("");
+    }
+
+    @FXML
+    private void processSetSave(ActionEvent event) {
+
+        String fPath = "";
+        fPath = new File("").getAbsolutePath();
+        fPath = fPath+"\\appscool.ini";
+
+
+        try {
+            //Создаем объект свойст
+            Properties properties = new Properties();
+            File file = new File(fPath);
+            //Загружаем свойства из файла
+            properties.load(new FileInputStream(file));
+            //Получаем в переменную значение конкретного свойства
+            String host = properties.getProperty("host");
+            //Устанавливаем значение свойста
+            properties.setProperty("host", "localhost:8080");
+            //Сохраняем свойства в файл.
+            properties.store(new FileOutputStream(file), null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JOptionPane.showMessageDialog(null, "Сохранили!");
+    }
+
+    @FXML
+    private void processExit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void proccessLoad(ActionEvent event) {
+        JOptionPane.showMessageDialog(null, "Загрузили!");
+    }
 }
--- src/appscool/Main.java
+++ src/appscool/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
-        primaryStage.setTitle("Хочу в школу!");
+        primaryStage.setTitle("Хочу в школу!!!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
