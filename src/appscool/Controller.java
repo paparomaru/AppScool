9227a4c4cfc8ee4e8ad47ba82515ba4cc2a32048
vinokurov63 <vinokurov63@gmail.com>
2017-11-27 23:11:35 +0400

    Сохранение параметров (запись в файл ини)

--- src/appscool/Controller.java
+++ src/appscool/Controller.java
@@ -12,6 +12,8 @@ import java.util.Properties;
 
 public class Controller {
 
+    private String fPath = "";
+
     @FXML
     public TextField tfFamilia;
 
@@ -23,7 +25,8 @@ public class Controller {
     @FXML
     private void processSetSave(ActionEvent event) {
 
-        String fPath = "";
+        String strLastName = "";
+
         fPath = new File("").getAbsolutePath();
         fPath = fPath+"\\appscool.ini";
 
@@ -35,9 +38,12 @@ public class Controller {
             //Загружаем свойства из файла
             properties.load(new FileInputStream(file));
             //Получаем в переменную значение конкретного свойства
-            String host = properties.getProperty("host");
+            String host = properties.getProperty("lastname");
+
+            strLastName = tfFamilia.getText();
+
             //Устанавливаем значение свойста
-            properties.setProperty("host", "localhost:8080");
+            properties.setProperty("lastname", strLastName);
             //Сохраняем свойства в файл.
             properties.store(new FileOutputStream(file), null);
         } catch (IOException e) {
@@ -54,6 +60,26 @@ public class Controller {
 
     @FXML
     private void proccessLoad(ActionEvent event) {
+
+        String strLastName = "";
+
+        fPath = new File("").getAbsolutePath();
+        fPath = fPath+"\\appscool.ini";
+
+
+        try {
+            //Создаем объект свойст
+            Properties properties = new Properties();
+            File file = new File(fPath);
+            //Загружаем свойства из файла
+            properties.load(new FileInputStream(file));
+            //Получаем в переменную значение конкретного свойства
+            strLastName = properties.getProperty("lastname");
+            tfFamilia.setText(strLastName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         JOptionPane.showMessageDialog(null, "Загрузили!");
     }
 }
